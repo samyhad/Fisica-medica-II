3ca72cb4dcf0fe657f61e6a0e8894ed9a2e80d09
Samira Haddad <samira.had.2000@gmail.com>
2021-11-16 01:49:15 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,24 +1,26 @@
 function main() 
   #DESCRIPTION: fft2 example
   #AUTHOR: Samira Haddad
-  #DATE: 2020/11/22
-  #IN: void
-  #OUT: void
+  #DATE: 2021/11/15
+  #IN: rawdata.m
+  #OUT: imagem resultante da transformada de fourier
   
-  dados = load(“rawdata.mat”) #carregando o arquivo que contém nossas fatias
-  rawdata = zeros(128:128:27)
+  dados = load("rawdata.mat"); #carregando o arquivo que contém nossas fatias
+  cell = struct2cell(dados);
+  rawdata = zeros(128,128,27);
   
-  for(i=1:1:27) # loop para selecionar cada uma das fatias
-    for(l=1:1:128) # loop para preencher as linhas da matriz
-      for(c = 1:1:128) # loop para preencher as colunas da matriz
-           
-        
+  for(fatia=1:1:27) # loop para selecionar cada uma das fatias
+    for(linha=1:1:128) # loop para preencher as linhas de cell
+      for(coluna = 1:1:128) # loop para preencher as colunas de cell
+           rawdata(linha, coluna, fatia) = cell{1,1}(linha, coluna, fatia);
       endfor;
     endfor;
   endfor;
   
-  #processado a imagem # 13
-  #figure;imagesc(abs(rawdata(:, :, 13)));colormap gray; axis image;colorbar; caxis([0 3e4])
+  #processando a imagem relativa a fatia # 13
+  figure;imagesc(abs(fft2(rawdata(:, :, 12))));
+  
+  
   
 endfunction
   
